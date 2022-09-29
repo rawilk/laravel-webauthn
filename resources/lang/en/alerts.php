916170c30b5b161a43b28d4984d7593ce0472a11
Randall Wilk <wilk.randall@gmail.com>
2022-09-29 09:44:36 -0500

    Add more error language lines

--- resources/lang/en/alerts.php
+++ resources/lang/en/alerts.php
@@ -8,4 +8,11 @@ return [
     'key_not_allowed_error' => 'The operation either timed out or was not allowed.',
     'key_already_used' => "This key is already registered. It's not necessary to register it again.",
     'login_not_allowed_error' => 'Please try again or use a different authentication method.',
+
+    'auth' => [
+        'AbortError' => 'Authentication ceremony was sent an abort signal.',
+        'NotAllowedError' => 'User clicked cancel, or the authentication ceremony timed out.',
+        'NotAllowedError_none_registered' => 'No available authenticator recognized any of the allowed credentials.',
+        'UnknownError' => 'The authenticator was unable to process your request.',
+    ],
 ];
