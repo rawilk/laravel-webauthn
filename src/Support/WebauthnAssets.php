344ea7d3ba16e62bdcdfbd23cd50db8036a4b44a
Randall Wilk <22842525+rawilk@users.noreply.github.com>
2023-04-14 09:23:12 -0500

    Content Security Policy Support (#16)

--- src/Support/WebauthnAssets.php
+++ src/Support/WebauthnAssets.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Rawilk\Webauthn\Support;
 
+use Illuminate\Support\Facades\Vite;
+
 class WebauthnAssets
 {
     public function javaScript(array $options = []): string
@@ -17,15 +19,35 @@ class WebauthnAssets
 
     private function javaScriptAssets(array $options = []): string
     {
-        $appUrl = config('webauthn.asset_url', rtrim($options['asset_url'] ?? '', '/'));
+        $assetsUrl = config('webauthn.asset_url') ?: rtrim($options['asset_url'] ?? '', '/');
+        $nonce = $this->getNonce($options);
 
         $manifest = json_decode(file_get_contents(__DIR__ . '/../../dist/mix-manifest.json'), true);
         $versionedFileName = ltrim($manifest['/assets/webauthn.js'], '/');
 
-        $fullAssetPath = "{$appUrl}/webauthn/{$versionedFileName}";
+        $fullAssetPath = "{$assetsUrl}/webauthn/{$versionedFileName}";
 
         return <<<HTML
-        <script src="{$fullAssetPath}" data-turbolinks-eval="false" data-turbo-eval="false"></script>
+        <script src="{$fullAssetPath}" data-turbolinks-eval="false" data-turbo-eval="false" {$nonce}></script>
         HTML;
+    }
+
+    private function getNonce(array $options): string
+    {
+        if (isset($options['nonce'])) {
+            return "nonce=\"{$options['nonce']}\"";
+        }
+
+        // If there is a csp package installed, i.e. spatie/laravel-csp, we'll check for the existence of the helper function.
+        if (function_exists('csp_nonce') && $nonce = csp_nonce()) {
+            return "nonce=\"{$nonce}\"";
+        }
+
+        // Lastly, we'll check for the existence of a csp nonce from Vite.
+        if (class_exists(Vite::class) && $nonce = Vite::cspNonce()) {
+            return "nonce=\"{$nonce}\"";
+        }
+
+        return '';
     }
 }
